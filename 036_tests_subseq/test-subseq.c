0c41e5896ac5c8f1659102e745e0dd6bb76b05e1
yl506 <yl506@duke.edu>
2017-09-13 10:22:49 -0400

    done

---
+++ 036_tests_subseq/test-subseq.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t maxSeq(int *array, size_t n);
+
+void helper(int * array, size_t n, size_t ans) {
+  
+  if (maxSeq(array,n) != ans){
+    exit(EXIT_FAILURE);
+  }
+}
+
+int main (void)
+{
+  int array1[0] = {};
+  helper(array1, 0, 0);
+  int array2[5] = {0,2,1,2,3};
+  helper(array2, 5, 3);
+  int array3[2] = {1,1};
+  helper(array3, 2, 1);
+  int array4[6] = {1,2,3,-3,-2,-1};
+  helper(array4, 6, 3);
+  return EXIT_SUCCESS;
+}
