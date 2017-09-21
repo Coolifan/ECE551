0158f7a7b2a1e8e5f2de6578458ece1563423d91
yl506 <yl506@duke.edu>
2017-09-21 19:52:07 -0400

    with comments

--- 056_kvs/kv.c
+++ 056_kvs/kv.c
@@ -5,25 +5,26 @@
 
 
 kvpair_t * readAPair(FILE* f) {
-  kvpair_t * kv = malloc(sizeof(*kv));
+  kvpair_t * kv = malloc(sizeof(*kv)); // the ptr to return kvpair struct
   size_t sz = 0;
-  char * line = NULL;
-  ssize_t len = getline(&line, &sz, f);
-  int equalsign = 0;
-  kv->key = calloc((len+1), sizeof(*kv->key));
+  char * line = NULL; //buffer
+  ssize_t len = getline(&line, &sz, f); // read one line and save to buffer
+  int equalsign = 0; // use to check if encounter the first = sign
+  kv->key = calloc((len+1), sizeof(*kv->key)); // not sure if len w/ +1
   kv->value = calloc((len+1), sizeof(*kv->value));
+  // allocate space for key and value
   for (int i = 0; i < len - 1; i++) {
     if (line[i] == '=' && equalsign == 0) {
-      equalsign = i;
+      equalsign = i; // if encounter 1st = sign, set counter, no write
     }
     else if (line[i] != '=' && equalsign == 0) {
-      kv->key[i] = line[i];
+      kv->key[i] = line[i]; // if haven't seen 1st =, keep writing to key
     }
-    else if (equalsign != 0) {
+    else if (equalsign != 0) { 
       kv->value[i-equalsign-1] = line[i];
-    }
+    } // if passed 1st = sign, write everything to value
   }
-  free(line);
+  free(line); //free mem allocated by getline
   return kv;
 }
 
@@ -35,24 +36,23 @@ kvarray_t * readKVs(const char * fname) {
     return NULL;
   }
   int paircnt = 0;
-  int c;
+  int c; // to count total # of lines(kv pairs) by counting \n
   while ((c = fgetc(f)) != EOF) {
     if (c == '\n') {
       paircnt++;
     }
   }
-  fseek(f, 0, SEEK_SET);
-  kvarray_t *kvary = malloc(sizeof(*kvary));
+  fseek(f, 0, SEEK_SET); //IMPORTANT
+  kvarray_t *kvary = malloc(sizeof(*kvary)); //the return kvarray
   kvary->length = paircnt;
-  kvary->pair = malloc(kvary->length * sizeof(*kvary->pair));
-
+  kvary->pair = malloc(kvary->length * sizeof(*kvary->pair)); 
+  //allocate mem for (length) kvpair 's
   for (int i = 0; i < kvary->length; i++) {
-    kvary->pair[i] = readAPair(f);
+    kvary->pair[i] = readAPair(f); // call fxn to read kv 's
   }
   
   fclose(f);
   return kvary; 
-  
 }
 
 void freeKVs(kvarray_t * pairs) {
@@ -60,11 +60,10 @@ void freeKVs(kvarray_t * pairs) {
   for (int i = 0; i < pairs->length; i++) {
     free((pairs->pair[i])->key);
     free((pairs->pair[i])->value);
-    free(pairs->pair[i]);
+    free(pairs->pair[i]); //after freeing key&value, FILO
   }
   free(pairs->pair);
   free(pairs);
-    
   
 }
 
@@ -77,11 +76,10 @@ void printKVs(kvarray_t * pairs) {
 
 char * lookupValue(kvarray_t * pairs, const char * key) {
   //WRITE ME
-  int j;
   for (int i = 0; i < pairs->length; i++) {
-    if ((j = strcmp(key, (pairs->pair[i])->key)) == 0) {
+    if ((strcmp(key, (pairs->pair[i])->key)) == 0) {
       return (pairs->pair[i])->value;
     }
-  }
+  } // it match, return value. Return NULL otherwise.
   return NULL;
 }
