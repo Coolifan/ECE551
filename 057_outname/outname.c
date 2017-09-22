157d4b862287494dbdc2ce1255de77ef1a34172a
yl506 <yl506@duke.edu>
2017-09-21 20:35:40 -0400

    done

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -5,4 +5,10 @@
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  const char * suffix = ".counts";
+  unsigned len = strlen(inputName) + strlen(suffix) + 1;
+  char * outputName = malloc(len * sizeof(*outputName));
+  snprintf(outputName, len, "%s%s", inputName, suffix);
+
+  return outputName;
 }
