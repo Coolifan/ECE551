ba84c6060b8a54276ae5a5d302598c82edf5ea6d
yl506 <yl506@duke.edu>
2017-10-08 15:32:28 -0400

    step 1 done

--- 060_stat/mystat.c
+++ 060_stat/mystat.c
@@ -19,4 +19,42 @@ char * time2str(const time_t * when, long ns) {
   return ans;
 }
 
+// BEGIN
+//Step1 : make mystat accept exactly 1 filename as a command line argument,
+// and print out the first 3 lines of output that "stat" would print 
+int main (int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
 
+  struct stat sb;
+  if (stat(argv[1], &sb) == -1) {
+    perror("stat");
+    exit(EXIT_FAILURE);
+  }
+
+
+  
+  printf("  File: '%s'\n", argv[1]);
+  printf("  Size: %-10lu\tBlocks: %-10lu IO Block: %-6lu", (long unsigned)sb.st_size, (long unsigned)sb.st_blocks, (long unsigned)sb.st_blksize);
+  switch (sb.st_mode & S_IFMT) {
+  case S_IFBLK: printf(" block special file\n");
+    break;
+  case S_IFCHR: printf(" character special file\n");
+    break;
+  case S_IFDIR: printf(" directory\n");
+    break;
+  case S_IFIFO: printf(" fifo\n");
+    break;
+  case S_IFLNK: printf(" symbolic link\n");
+    break;
+  case S_IFREG: printf(" regular file\n");
+    break;
+  default: printf(" socket\n");
+    break;
+  }
+
+  printf("Device: %lxh/%lud\tInode: %-10lu  Links: %lu\n", (unsigned long)sb.st_dev, (long unsigned)sb.st_dev, (long unsigned)sb.st_ino, (long unsigned)sb.st_nlink);
+  return EXIT_SUCCESS;
+}
