11a53f8a6f0c57bb1a981702ee538dc540fc3fc9
yl506 <yl506@duke.edu>
2017-10-10 03:49:57 -0400

    wokring

--- 060_stat/mystat.c
+++ 060_stat/mystat.c
@@ -19,10 +19,30 @@ char * time2str(const time_t * when, long ns) {
   return ans;
 }
 
-/*Step1 : make mystat accept exactly 1 filename as a command line argument,
- and print out the first 3 lines of output that "stat" would print */
+/*Step 1 : print out the first 3 lines of output that "stat" would print */
+void regularfile(struct stat sb) {
+  if (sb.st_size == 0 && sb.st_blocks == 0) {
+    printf(" regular empty file\n");
+  }
+  else {
+    printf(" regular file\n");
+  }
+}
 void printfirst3lines(char *filename, struct stat sb) {
-  printf("  File: '%s'\n", filename);
+
+  if (S_ISLNK(sb.st_mode)) {
+    char linktarget[256];
+    ssize_t len = readlink(filename, linktarget, 256);
+    if (len < 0) {
+      perror("lstat");
+      exit(EXIT_FAILURE);
+    }
+    linktarget[len] = '\0';
+    printf("  File: '%s' -> '%s'\n", filename, linktarget);
+  }
+  else if (!S_ISLNK(sb.st_mode)) {
+    printf("  File: '%s'\n", filename);
+  }
   printf("  Size: %-10lu\tBlocks: %-10lu IO Block: %-6lu", (long unsigned)sb.st_size, (long unsigned)sb.st_blocks, (long unsigned)sb.st_blksize);
   switch (sb.st_mode & S_IFMT) {
   case S_IFBLK: printf(" block special file\n");
@@ -35,17 +55,24 @@ void printfirst3lines(char *filename, struct stat sb) {
     break;
   case S_IFLNK: printf(" symbolic link\n");
     break;
-  case S_IFREG: printf(" regular file\n");
+  case S_IFREG: regularfile(sb); 
     break;
   case S_IFSOCK: printf(" socket\n");
     break;
   default: printf(" unknown?\n");
-    break;
   }
-  printf("Device: %lxh/%lud\tInode: %-10lu  Links: %lu\n", (unsigned long)sb.st_dev, (long unsigned)sb.st_dev, (long unsigned)sb.st_ino, (long unsigned)sb.st_nlink);
-}
 
-/* Step2 : add part of the 4th line of output from stat*/
+  /*Step 6: fix the discrepancy of line 3*/
+  if (S_ISCHR(sb.st_mode) || S_ISBLK(sb.st_mode)) {
+    printf("Device: %lxh/%lud\tInode: %-10lu  Links: %-5lu Device type: %x,%x\n", (unsigned long)sb.st_dev, (unsigned long)sb.st_dev, (unsigned long)sb.st_ino,
+	   (unsigned long)sb.st_nlink, major(sb.st_rdev), minor(sb.st_rdev));
+  }
+  else {
+    printf("Device: %lxh/%lud\tInode: %-10lu  Links: %lu\n", (unsigned long)sb.st_dev, (long unsigned)sb.st_dev, (long unsigned)sb.st_ino,
+	   (long unsigned)sb.st_nlink);
+  }
+}
+/* Step 2 : add part of the 4th line of output from stat*/
 void printline4(char *filename, struct stat sb) {
   printf("Access: (%04o/", sb.st_mode & (~S_IFMT));
 
@@ -80,13 +107,19 @@ void printline4(char *filename, struct stat sb) {
     printf("-");
   }
 
-  if ((sb.st_mode & S_IXUSR) != 0) {
+  if ((sb.st_mode & S_ISUID) != 0 && (sb.st_mode & S_IXUSR) != 0) {
+    printf("s");
+  }
+  else if ((sb.st_mode & S_ISUID) != 0  && (sb.st_mode & S_IXUSR) == 0) {
+    printf("S");
+  }
+  else if ((sb.st_mode & S_ISUID) == 0 && (sb.st_mode & S_IXUSR) != 0) { 
     printf("x");
   }
   else {
     printf("-");
   }
-
+  
   if ((sb.st_mode & S_IRGRP) != 0) {
     printf("r");
   }
@@ -101,7 +134,13 @@ void printline4(char *filename, struct stat sb) {
     printf("-");
   }
 
-  if ((sb.st_mode & S_IXGRP) != 0) {
+  if ((sb.st_mode & S_ISGID) != 0 && (sb.st_mode & S_IXGRP) != 0) {
+    printf("s");
+  }
+  else if ((sb.st_mode & S_ISGID) != 0 && (sb.st_mode & S_IXGRP) == 0) {
+    printf("S");
+  }
+  else if ((sb.st_mode & S_ISGID) == 0 && (sb.st_mode & S_IXGRP) != 0) {
     printf("x");
   }
   else {
@@ -122,36 +161,70 @@ void printline4(char *filename, struct stat sb) {
     printf("-");
   }
   
-  if ((sb.st_mode & S_IXOTH) != 0) {
+
+  if ((sb.st_mode & S_ISVTX) != 0 && (sb.st_mode & S_IXOTH) != 0) {
+    printf("t");
+  }
+  else if ((sb.st_mode & S_ISVTX) != 0 && (sb.st_mode & S_IXOTH) == 0) {
+    printf("T");
+  }
+  else if ((sb.st_mode & S_ISVTX) == 0 && (sb.st_mode & S_IXOTH) != 0) {
     printf("x");
   }
   else {
     printf("-");
   }
-
+  
   printf(")  ");
 
   /* Step 3: finish the 4th line*/
   struct passwd * pw = getpwuid(sb.st_uid);
   struct group * grp = getgrgid(sb.st_gid);
+
+  if (pw == NULL) {
+    perror("getpwuid");
+    exit(EXIT_FAILURE);
+  }
+  if (grp == NULL) {
+    perror("getgrgid");
+    exit(EXIT_FAILURE);
+  }	
   
   printf("Uid: (%5d/%8s)   Gid: (%5d/%8s)\n", sb.st_uid, pw->pw_name, sb.st_gid, grp->gr_name);
-
-
 }
+/* Step 4: add the last 4 lines of output */
+void printlast4lines(char *filename, struct stat sb) {
+  char *Atimestr = time2str(&sb.st_atime, sb.st_atim.tv_nsec);
+  char *Mtimestr = time2str(&sb.st_mtime, sb.st_mtim.tv_nsec);
+  char *Ctimestr = time2str(&sb.st_ctime, sb.st_ctim.tv_nsec);
+
+  printf("Access: %s\n", Atimestr);
+  printf("Modify: %s\n", Mtimestr);
+  printf("Change: %s\n", Ctimestr);
+
+  printf(" Birth: -\n");
+  
+  free(Atimestr);
+  free(Mtimestr);
+  free(Ctimestr);
+}
+
 int main (int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
+  if (argc < 2) {
+    fprintf(stderr, "stat: missing operand\n");
     exit(EXIT_FAILURE);
   }
 
   struct stat sb;
-  if (stat(argv[1], &sb) == -1) {
-    perror("stat");
-    exit(EXIT_FAILURE);
+  for (int i = 1; i < argc; i++) {
+    if (lstat(argv[i], &sb) == -1) {
+      fprintf(stderr, "stat: cannot stat '%s': No such file or directory\n", argv[i]);
+      exit(EXIT_FAILURE);
+    }
+    
+    printfirst3lines(argv[i], sb); 
+    printline4(argv[i], sb); 
+    printlast4lines(argv[i], sb); 
   }
-  printfirst3lines(argv[1], sb); // calling step 1
-  printline4(argv[1], sb); //calling step 2
-  
   return EXIT_SUCCESS;
 }
