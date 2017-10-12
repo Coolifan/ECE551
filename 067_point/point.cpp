cce0b343417365655634e0dca7679e2d0cbf0566
yl506 <yl506@duke.edu>
2017-10-11 21:37:45 -0400

    done

---
+++ 067_point/point.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include "point.h"
+Point::Point() : x(0.0), y(0.0) {
+
+}
+
+void Point::move(double dx, double dy) {
+  x += dx;
+  y += dy;
+}
+
+double Point::distanceFrom(const Point & p) {
+  return sqrt( (x-p.x)*(x-p.x) + (y-p.y)*(y-p.y) );
+}
