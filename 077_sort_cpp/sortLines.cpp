b011f1055490910b2fba9d6c9ad0e2dbe6768bd6
yl506 <yl506@duke.edu>
2017-10-17 15:40:37 -0400

    done

---
+++ 077_sort_cpp/sortLines.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+
+int main(int argc, char *argv[]) {
+  std::string line;
+  std::vector<std::string> v;
+  
+  if (argc == 1) {
+    while (std::getline(std::cin, line)) {
+      v.push_back(line);
+    }
+    std::sort(v.begin(), v.end());
+    for (size_t i = 0; i < v.size(); i++) {
+      std::cout << v[i] << std::endl;
+    }
+  }
+  
+  else {
+    for (int i = 1; i < argc; i++) {
+      std::ifstream input;
+      input.open(argv[i]);
+      std::vector<std::string> v2;
+      while (std::getline(input, line)) {
+	v2.push_back(line);
+      }
+      std::sort(v2.begin(), v2.end());
+      for (size_t j = 0; j < v2.size(); j++) {
+	std::cout << v2[j] << std::endl;
+      }
+      input.close();
+    }
+    
+  }
+
+  return EXIT_SUCCESS;
+}
