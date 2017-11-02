6f48d859568dc6c786b82ac070b9fdfad60c8584
yl506 <yl506@duke.edu>
2017-11-02 00:44:49 -0400

    eh

---
+++ 093_binsrch/search.cpp
@@ -0,0 +1,26 @@
+#include<cstdlib>
+#include<iostream>
+#include"function.h"
+#include<cstdio>
+
+int binarySearchForZero(Function<int, int> * f, int low, int high) {
+  if (low == high) {
+    return low;
+  }
+  if (f->invoke(low) < 0 && f->invoke(high) < 0) {
+    return high-1;
+  }
+  if (f->invoke(low) > 0 && f->invoke(high) > 0) {
+    return low;
+  }
+  int mid = low + (high - low) / 2;
+  if (f->invoke(mid) > 0) {
+    return binarySearchForZero(f, low, mid);
+  }
+  else if (f->invoke(mid) < 0) {
+    return binarySearchForZero(f, mid, high);
+  }
+  else{
+    return mid;
+  }
+}
