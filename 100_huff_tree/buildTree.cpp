bec5296fd25a129c39cacfaadd6172e422d95f36
yl506 <yl506@duke.edu>
2017-11-08 14:49:02 -0500

    done

--- 100_huff_tree/buildTree.cpp
+++ 100_huff_tree/buildTree.cpp
@@ -2,4 +2,18 @@
 
 Node *  buildTree(uint64_t * counts) {
   //WRITE ME!
+  priority_queue_t pq;
+  for (int i = 0; i < 257; i++) {
+    if (counts[i] > 0) {
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+  while (pq.size() > 1) {
+    Node* dequed1 = pq.top();
+    pq.pop();
+    Node* dequed2 = pq.top();
+    pq.pop();
+    pq.push(new Node(dequed1, dequed2));
+  }
+  return pq.top();
 }  
